4411d6be0ec0f88926f4d4f458391578caaa7df1
Thibaut LD <t.ledoare@rubika-edu.com>
2021-03-11 12:21:11 +0100

    traps

--- SI Machine Learning/Assets/Script/Boost.cs
+++ SI Machine Learning/Assets/Script/Boost.cs
@@ -7,6 +7,7 @@ public class Boost : MonoBehaviour
     public LayerMask LayerMask;
     public int BoostValue = 200;
     public int maxBoostValue = 700;
+    public int BoostDuration = 5;
 
     private void OnTriggerEnter(Collider col)
     {
@@ -23,7 +24,7 @@ public class Boost : MonoBehaviour
         if (Go.GetComponentInParent<CarControler>().motorForce < maxBoostValue)
         {
             Go.GetComponentInParent<CarControler>().motorForce += BoostValue;
-            yield return new WaitForSeconds(5);
+            yield return new WaitForSeconds(BoostDuration);
             Go.GetComponentInParent<CarControler>().motorForce = val;
         }
 
---
+++ SI Machine Learning/Assets/Script/Trap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public LayerMask LayerMask;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == 10 )
+        {
+            crash(col.gameObject);
+        }
+    }
+
+    void crash(GameObject Go)
+    {
+        Go.GetComponentInParent<CarControler>().motorForce = 0;        
+    }
+}
